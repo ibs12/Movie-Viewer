c0b0c71e900a0aa6fe39b3a94527acdf965b3aa4
ibs12 <ibrahimallahb@gmail.com>
2022-09-20 02:05:48 -0400

    Added Part 2

---
+++ IOS Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  IOS Flix
+//
+//  Created by Ibrahim  Allahbuksh on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}

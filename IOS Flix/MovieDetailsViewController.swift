6ff263e8f299647e45e4f38a7c2343108d624961
ibs12 <89870752+ibs12@users.noreply.github.com>
2023-07-08 23:38:02 -0400

    Implemented Local Database, user can now view list of now playing movie offline

--- IOS Flix/MovieDetailsViewController.swift
+++ IOS Flix/MovieDetailsViewController.swift
@@ -16,46 +16,31 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
     
-    
-    var movie: [String:Any]!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        titleLabel.lineBreakMode = .byWordWrapping
-
-        
-        titleLabel.text = movie["title"] as? String
-        titleLabel.sizeToFit()
-        synopsisLabel.text = movie["overview"] as? String
-        synopsisLabel.sizeToFit()
-        
-        let baseURL = "https://image.tmdb.org/t/p/w185"
-        let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseURL + posterPath)
-        
-        
-        posterView.af.setImage(withURL: posterUrl!)
-        
- 
-        
-        let backdropPath = movie["backdrop_path"] as! String
-        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
-         
-        
-        backdropView.af.setImage(withURL: backdropUrl!)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    var movie: [String: Any]?
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           titleLabel.lineBreakMode = .byWordWrapping
+           
+           if let movie = movie {
+               titleLabel.text = movie["title"] as? String
+               titleLabel.sizeToFit()
+               synopsisLabel.text = movie["overview"] as? String
+               synopsisLabel.sizeToFit()
+               
+               let baseURL = "https://image.tmdb.org/t/p/w185"
+               if let posterPath = movie["poster_path"] as? String,
+                  let posterURL = URL(string: baseURL + posterPath) {
+                   posterView.af.setImage(withURL: posterURL)
+               }
+               
+               if let backdropPath = movie["backdrop_path"] as? String,
+                  let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath) {
+                   backdropView.af.setImage(withURL: backdropURL)
+               }
+           }
+       }
+       
+       // ...
+   }
--- IOS Flix/MoviesViewController.swift
+++ IOS Flix/MoviesViewController.swift
@@ -18,43 +18,94 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movies = [[String:Any]]()
     var managedObjectContext: NSManagedObjectContext!
+    var hasFetchedMovies = false
+
 
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-   
-        view.backgroundColor = .black
-        tableView.backgroundColor = .black
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-             // This will run when the network request returns
-             if let error = error {
-                    print(error.localizedDescription)
-             } else if let data = data {
-                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                 
-                 
-                 self.movies = dataDictionary["results"] as! [[String:Any]]
-                 self.tableView.reloadData()
-                 print(dataDictionary)
-
-                    // TODO: Get the array of movies
-                    // TODO: Store the movies in a property to use elsewhere
-                    // TODO: Reload your table view data
-
-             }
+            super.viewDidLoad()
             
+
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            managedObjectContext = appDelegate.persistentContainer.viewContext
+
+            view.backgroundColor = .black
+            tableView.backgroundColor = .black
+            navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+            tableView.dataSource = self
+            tableView.delegate = self
+            
+            let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                     self.tableView.reloadData()
+                } else if let data = data {
+                    do {
+                        if let dataDictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            self.movies = dataDictionary["results"] as? [[String:Any]] ?? []
+                            for movie in self.movies {
+                                self.saveMovieToLocalDatabase(movie: movie)
+                            }
+                            self.hasFetchedMovies = true  // Set the flag here
+                            self.tableView.reloadData()
+                        }
+                    } catch {
+                        print("Error during JSON serialization: \(error.localizedDescription)")
+                    }
+                }
+            }
+            task.resume()
             
         }
-        task.resume()
-        
+    
+    func saveMovieToLocalDatabase(movie: [String: Any]) {
+        let entity = NSEntityDescription.entity(forEntityName: "MovieEntity", in: managedObjectContext!)
+        let newMovie = NSManagedObject(entity: entity!, insertInto: managedObjectContext)
+
+        newMovie.setValue(movie["id"], forKey: "id")
+        newMovie.setValue(movie["title"], forKey: "title")
+        newMovie.setValue(movie["overview"], forKey: "overview")
+        newMovie.setValue(movie["poster_path"], forKey: "poster_path")
+
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Failed saving")
+        }
     }
+    
+    func fetchMoviesFromLocalDatabase() -> [[String: Any]] {
+        var movies = [[String: Any]]()
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "MovieEntity")
+
+        do {
+            let fetchedMovies = try managedObjectContext.fetch(fetchRequest)
+            for movie in fetchedMovies {
+                var movieDict = [String: Any]()
+                movieDict["id"] = movie.value(forKey: "id")
+                movieDict["title"] = movie.value(forKey: "title")
+                movieDict["overview"] = movie.value(forKey: "overview")
+                movieDict["poster_path"] = movie.value(forKey: "poster_path")
+                movies.append(movieDict)
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+
+        return movies
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -92,27 +143,38 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        // Get the new view  controller using segue.destination
-        // Pass the selected object to the new view controller.
-        
-        print("Loading up the details screen")
-        
-        //find the selected movie
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: cell)!
-        let movie = movies[indexPath.row]
-        
-        
-        
-        //Pass the selected movie to the details view controller
-        let detailsViewController = segue.destination as! MovieDetailsViewController
-        
-        detailsViewController.movie = movie
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            print("Loading up the details screen")
+            
+            // Find the selected movie
+            let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPath(for: cell)!
+            var movie = movies[indexPath.row]
+            
+            // If we're offline, fetch the movie details from the local database instead
+            if !hasFetchedMovies {
+                let id = movie["id"] as? Int64 ?? 0
+                let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "MovieEntity")
+                fetchRequest.predicate = NSPredicate(format: "id == %@", id)
+                
+                do {
+                    let fetchedMovies = try managedObjectContext.fetch(fetchRequest)
+                    if let fetchedMovie = fetchedMovies.first {
+                        movie["overview"] = fetchedMovie.value(forKey: "overview")
+                        movie["poster_path"] = fetchedMovie.value(forKey: "poster_path")
+                        // add any other details you're storing in the database
+                    }
+                } catch let error as NSError {
+                    print("Could not fetch. \(error), \(error.userInfo)")
+                }
+            }
+            
+            // Pass the selected movie to the details view controller
+            let detailsViewController = segue.destination as! MovieDetailsViewController
+            detailsViewController.movie = movie
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+
         
     }
-    
-}
